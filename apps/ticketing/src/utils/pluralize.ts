c5d91bc7f5f1aac08c243230735c9a20f2896b1d
charles <charleslavong@gmail.com>
2024-12-05 16:14:00 -0500

    remove the ticketing project

--- apps/ticketing/src/utils/pluralize.ts
+++
@@ -1,4 +0,0 @@
-export function pluralize(count: number | null | undefined, wordSingle: string, wordPlural?: string) {
-  if (count === 1) return wordSingle;
-  return wordPlural || wordSingle + 's';
-}
